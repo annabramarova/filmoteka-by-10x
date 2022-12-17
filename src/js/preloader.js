9ab3b9382f046a395af728e7f8ba0478e9d3461f
Anna Bramarova <annabramarova@gmail.com>
2022-12-17 18:38:56 +0200

    preloader

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,8 +1,8 @@
 document.body.onload = function () {
     setTimeout(function () {
-        const preloader = document.getElementById('preloader');
+        const preloader = document.querySelector('#preloader');
         if (!preloader.classList.contains('done')) {
             preloader.classList.add('done');
         }
-    }, 1000);
+    }, 1500);
 }
