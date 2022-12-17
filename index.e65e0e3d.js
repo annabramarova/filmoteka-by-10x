1fedfea29aa53e94a21fece5a6ce985645d34eb3
annabramarova <108080975+annabramarova@users.noreply.github.com>
2022-12-17 16:39:20 +0000

    Deploying to gh-pages from @ annabramarova/filmoteka-by-10x@9ab3b9382f046a395af728e7f8ba0478e9d3461f 🚀

---
+++ index.e65e0e3d.js
@@ -0,0 +1,2 @@
+document.body.onload=function(){setTimeout((function(){const o=document.querySelector("#preloader");o.classList.contains("done")||o.classList.add("done")}),1500)};
+//# sourceMappingURL=index.e65e0e3d.js.map
---
+++ index.f8f8acb5.js
@@ -0,0 +1,2 @@
+document.body.onload=function(){setTimeout((function(){var o=document.querySelector("#preloader");o.classList.contains("done")||o.classList.add("done")}),1500)};
+//# sourceMappingURL=index.f8f8acb5.js.map
