65a9c2795033bc9f463deebaf513297b0de2ee91
sofiagakalo1 <sofiagakalo1@gmail.com>
2022-12-20 19:53:58 +0100

    render

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/preloader';
+import './js/render-gallery';
 import './js/team-modal';
 import './js/loading';
--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -1,7 +1,7 @@
-import {galleryTemplate} from './gallery';
+import { galleryTemplate } from './gallery';
 import { refs } from './refs';
 
-export function render(movies){
-    const moviesCollection = movies.results.map(galleryTemplate).join('');
-refs.galleryList.innerHTML = moviesCollection;
+export function render(movies) {
+  const moviesCollection = movies.results.map(galleryTemplate).join('');
+  refs.galleryList.innerHTML = moviesCollection;
 }
