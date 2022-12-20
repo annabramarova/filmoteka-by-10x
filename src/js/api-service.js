2ed3b6d1f5f6f0bd92d66ec8606ddbb91b9ebece
beligrous <beligrous@gmail.com>
2022-12-20 19:51:39 +0200

    refactor on class

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,22 +1,25 @@
 import axios from 'axios';
-// class Film {
-//   constructor() {}
-// }
+export class Api {
+  constructor() {
+    this.URL = 'https://api.themoviedb.org/3/';
+    this.KEY = 'c23d7755b502540a74ef819e02a6a593';
+    this.page = 1;
+    this.query = '';
+  }
 
-const vars = {
-  api_key: 'c23d7755b502540a74ef819e02a6a593',
-  URL: 'https://api.themoviedb.org/3/movie',
-  pagination: '&query=${}&page=${}&total_pages=${}',
-};
+  async getTrendingFilms() {
+    const r = await axios.get(
+      `${this.URL}/trending/movie/week?api_key=${this.KEY}&language=en-US&page=${this.page}`
+    );
+    const d = await axios.get(r.data);
+    return d;
+  }
 
-export async function trendFilms() {
-  // const b = await axios.get(
-  //   'https://api.themoviedb.org/3/movie/550?api_key=c23d7755b502540a74ef819e02a6a593'
-  // );
-  const a = await axios.get(`${vars.URL}?api_key=${vars.api_key}`);
-  console.log(a);
+  async getFilmBySearch() {}
 }
 
-console.log(vars.URL);
-console.log(vars.api_key);
-trendFilms();
+// const vars = {
+//   api_key: 'c23d7755b502540a74ef819e02a6a593',
+//   URL: 'https://api.themoviedb.org/3/',
+//   pagination: '&query=${}&page=${}&total_pages=${}',
+// };
