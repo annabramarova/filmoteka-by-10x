5ba76e54f7ee13ee8a31cee1084381a2db22efaa
Niko123454321 <kvbsnsk@gmail.com>
2022-12-20 22:11:18 +0200

    начало десктоп

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,4 @@
 export const refs = {
-    galleryList: document.querySelector('.collection_list'),
-}
+  galleryList: document.querySelector('.collection_list'),
+  pagin_box: document.querySelector('.pagin_box'),
+};
