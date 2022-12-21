cbd5c3e6b1162c9e2189bcc61c8aa7ac698f1658
beligrous <beligrous@gmail.com>
2022-12-21 18:33:50 +0200

    add getTrailer

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -7,6 +7,7 @@ export default class Api {
     this.KEY = 'c23d7755b502540a74ef819e02a6a593';
     this.page = 1;
     this.query = '';
+    this.id = 676547;
   }
 
   async getTrendingFilms() {
@@ -15,6 +16,7 @@ export default class Api {
         `/trending/movie/week?api_key=${this.KEY}&language=en-US&page=${this.page}`
       )
       .then(res => res.data);
+    console.log(r.results);
 
     return r.results;
   }
@@ -32,12 +34,22 @@ export default class Api {
   async getGenres() {
     const g = await axios
       .get(`/genre/movie/list?api_key=${this.KEY}&language=en-US`)
-      .then(r => r.data);
+      .then(r => data);
     const genres = g.genres;
     return genres;
+  }
+
+  async getTreiler() {
+    const r = await axios
+      .get(`/movie/${this.id}/videos?api_key=${this.KEY}&language=en-US`)
+      .then(r => r.data);
+    const trailer = r.results.filter(v => v.name === 'Official Trailer');
+    console.log(trailer[0]);
+    return trailer[0];
   }
 }
 
 // const films = new Api();
 // films.getTrendingFilms();
 // films.getGenres();
+// films.getTreiler();
