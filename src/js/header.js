4391c7cb1d08d4a3f62b9fd76f0efa8bcd194332
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-21 22:46:16 +0200

    21.12.2022 - 22:45

--- src/js/header.js
+++ src/js/header.js
@@ -4,10 +4,6 @@ refs.myLibraryLink.addEventListener('click', onMyLibraryClick);
 refs.headerButtons[0].addEventListener('click', onWatchedClick);
 refs.headerButtons[1].addEventListener('click', onQueueClick);
 
-// refs.watchedButton.addEventListener('click', e => console.log('CLICK!'));
-
-// refs.queueButton.addEventListener('click', e => console.log('CLICK!'));
-
 function onMyLibraryClick(e) {
   e.preventDefault();
 
--- src/js/refs.js
+++ src/js/refs.js
@@ -3,11 +3,8 @@ export const refs = {
   pagin_box: document.querySelector('.pagin_box'),
   formSearch: document.querySelector('.search-form'),
 
-  // logo: document.querySelector('.logo'),
   header: document.querySelector('.header'),
   homeLink: document.querySelector('#home-link'),
   myLibraryLink: document.querySelector('#my-library-link'),
-  // watchedButton: document.querySelector('[data-activ="watched"]'),
-  // queueButton: document.querySelector('[data-activ="queue"]'),
   headerButtons: document.querySelectorAll('.header-button'),
 };
