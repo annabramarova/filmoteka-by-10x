fdfffdd0c168dea45b8d60aedf11a35888f9b517
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-12-23 01:01:38 +0200

    adapt3

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,18 +1,13 @@
 import { loader, loaderRemove } from './loading';
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-team-modal-open]'),
-    closeModalBtn: document.querySelector('[data-team-modal-close]'),
-    modal: document.querySelector('[data-team-modal]'),
-    body: document.querySelector('body'),
-  };
+import { refs } from './refs';
 
+(() => {
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     loader();
-    refs.modal.classList.toggle('visually-hidden');
+    refs.modalTeam.classList.toggle('visually-hidden');
     loaderRemove();
     refs.body.classList.toggle('no-scroll');
   }
