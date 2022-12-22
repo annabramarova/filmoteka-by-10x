46d13d9d17b35e62948d61c4d681be4de7380be4
Anna Bramarova <annabramarova@gmail.com>
2022-12-22 16:36:12 +0200

    закоментировала код Вити

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
-import { getCardTemplate } from './getCardTemplate';
+// import axios from 'axios';
+// import { getCardTemplate } from './getCardTemplate';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-const API_KEY = 'e236468c654efffdf9704cd975a74a96';
+// axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+// const API_KEY = 'e236468c654efffdf9704cd975a74a96';
 
-async function fetchMovie(movieId) {
-  return await axios.get(`/movie/${movieId}?api_key=${API_KEY}&language=en-US`);
-}
+// async function fetchMovie(movieId) {
+//   return await axios.get(`/movie/${movieId}?api_key=${API_KEY}&language=en-US`);
+// }
 
-function renderCard() {
-  const cardMarkup = getCardTemplate();
-  const сard = document.querySelector('.movie-card');
-  сard.insertAdjacentHTML('beforeend', cardMarkup);
-}
+// function renderCard() {
+//   const cardMarkup = getCardTemplate();
+//   const сard = document.querySelector('.movie-card');
+//   сard.insertAdjacentHTML('beforeend', cardMarkup);
+// }
 
-fetchMovie(76600)
-  .then(({ data }) => renderCard(data))
-  .catch(() => []);
+// fetchMovie(76600)
+//   .then(({ data }) => renderCard(data))
+//   .catch(() => []);
