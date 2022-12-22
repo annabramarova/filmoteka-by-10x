ecd2228e72e6b6f79c475171b8d6c7530206353c
beligrous <beligrous@gmail.com>
2022-12-22 15:46:43 +0200

    new methods byPage

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -6,10 +6,8 @@ export default class Api {
   constructor() {
     this.KEY = 'c23d7755b502540a74ef819e02a6a593';
     this.page = 1;
-    // this.search = 'cat';
-    // this.query = '';
-    // this.IdMassive = [436270, 899112, 751741];
-    // this.id = 436270;
+    this.search = 'cat';
+    this.query = '';
   }
 
   async getTrendingFilms() {
@@ -21,14 +19,32 @@ export default class Api {
     return r.results;
   }
 
+  async getTrendingFilmsByPage(page) {
+    const r = await axios
+      .get(
+        `/trending/movie/week?api_key=${this.KEY}&language=en-US&page=${page}`
+      )
+      .then(res => res.data);
+    return r;
+  }
+
   async getFilmBySearch(search) {
     // this.query = search;
     const r = await axios
       .get(
-        `/search/movie?api_key=${this.KEY}&query=${search}&language=en-US&${this.page}`
+        `/search/movie?api_key=${this.KEY}&query=${search}&language=en-US&page=${this.page}`
       )
       .then(res => res.data);
     return r.results;
+  }
+
+  async getFilmSearchByPage(page) {
+    const r = await axios
+      .get(
+        `/search/movie?api_key=${this.KEY}&query=${this.query}&language=en-US&page=${page}`
+      )
+      .then(res => res.data);
+    return r;
   }
 
   async getFilmById(id) {
@@ -43,11 +59,6 @@ export default class Api {
     return res;
   }
 
-  getNextPage() {
-    let nextPage = this.page + 1;
-    return nextPage;
-  }
-
   async getGenres() {
     const g = await axios
       .get(`/genre/movie/list?api_key=${this.KEY}&language=en-US`)
@@ -56,9 +67,9 @@ export default class Api {
     return genres;
   }
 
-  async getTreiler() {
+  async getTrailer(id) {
     const r = await axios
-      .get(`/movie/${this.id}/videos?api_key=${this.KEY}&language=en-US`)
+      .get(`/movie/${id}/videos?api_key=${this.KEY}&language=en-US`)
       .then(r => r.data);
     const trailer = r.results.filter(v => v.name === 'Official Trailer');
     console.log(trailer[0]);
@@ -66,10 +77,10 @@ export default class Api {
   }
 }
 
-const films = new Api();
+// const films = new Api();
 // films.getTrendingFilms();
 // films.getGenres();
 // films.getTreiler();
 // films.getFilmById();
 // films.getFilmMassiveById();
-films.getFilmBySearch();
+// films.getFilmBySearch();
