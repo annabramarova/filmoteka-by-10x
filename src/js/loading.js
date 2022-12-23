87073bdf7ce1036e14915e8fa476b4b47283808d
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-12-23 03:17:49 +0200

    fix loading,ESC,font

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,12 +1,25 @@
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
 export function loader() {
-  Loading.arrows({
-    backgroundColor: 'rgba(0,0,0,0.2)',
-    svgColor: '#ff6b01',
-  });
+  const loader = document.querySelector('#preloader');
+  if (loader.classList.contains('done')) {
+    loader.classList.remove('done');
+    loader.style.backgroundColor = `#ffffff00`;
+  }
+
+  //notiflix
+  // Loading.arrows({
+  //   backgroundColor: 'rgba(0,0,0,0.2)',
+  //   svgColor: '#ff6b01',
+  // });
 }
 
 export function loaderRemove() {
-  Loading.remove();
+  const loader = document.querySelector('#preloader');
+  if (!loader.classList.contains('done')) {
+    loader.classList.add('done');
+  }
+
+  //notiflix
+  // Loading.remove();
 }
--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,4 +1,3 @@
-import { loader, loaderRemove } from './loading';
 import { refs } from './refs';
 
 (() => {
@@ -6,9 +5,16 @@ import { refs } from './refs';
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    loader();
     refs.modalTeam.classList.toggle('visually-hidden');
-    loaderRemove();
     refs.body.classList.toggle('no-scroll');
   }
 })();
+
+function offModal(e) {
+  if (e.currentTarget === e.target || e.code === 'Escape') {
+    refs.modalTeam.classList.add('visually-hidden');
+  }
+}
+
+refs.modalTeam.addEventListener('mousedown', offModal);
+document.addEventListener('keydown', offModal);
