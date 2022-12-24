312fd40d6d7ab6f5fa16d1fe5eaa2599bff9c63b
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-24 20:37:10 +0200

    markup +

--- src/js/refs.js
+++ src/js/refs.js
@@ -38,4 +38,7 @@ export const refs = {
   galleryQueueBtn: document.querySelector('button[data-activ="queue"]'),
   addToQueueBtn: document.querySelector(`.addToQueue`),
   galleryContainer: document.querySelector(`.gallery-container`),
+
+  // emty library-plug
+  plug: document.querySelector('.plug'),
 };
