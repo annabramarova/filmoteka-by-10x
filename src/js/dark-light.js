82a8c5c1be590034836e4d91ee5cb896ee1ee6fd
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-25 19:36:36 +0200

    19:36

--- src/js/dark-light.js
+++ src/js/dark-light.js
@@ -1,14 +1,15 @@
-// //!  refs
 import { refs } from './refs';
 
-// themeToggle.checked = true;
 const addDarkClassToHTML = () => {
   try {
-    localStorage.getItem('theme') === 'dark'
-      ? refs.HTML.classList.add('dark')
-      : refs.HTML.classList.remove('dark');
+    if (localStorage.getItem('theme') === 'dark') {
+      refs.HTML.classList.add('dark');
+      refs.themeToggle.checked = true;
+    } else {
+      refs.HTML.classList.remove('dark');
+    }
   } catch (err) {
-    console.log(err);
+    return;
   }
 };
 
@@ -23,30 +24,3 @@ const onThemeToggleClick = e => {
 refs.themeToggle.addEventListener('click', onThemeToggleClick);
 
 addDarkClassToHTML();
-
-// themeChange.addEventListener('click', () => {
-//
-//   localStorage.getItem('theme') === 'dark'
-//     ? localStorage.removeItem('theme')
-//     : localStorage.setItem('theme', 'dark');
-
-//   addDarkClassToHTML();
-// });
-
-// function addDarkClassToHTML() {
-//   try {
-//      if (localStorage.getItem('theme') === 'dark') {
-//        document.querySelector('html').classList.add('dark');
-//        //!  change icon
-//      } else {
-//        document.querySelector('html').classList.remove('dark');
-//        //!  change icon
-//      }
-
-//     localStorage.getItem('theme') === 'dark'
-//       ? document.querySelector('html').classList.add('dark')
-//       : document.querySelector('html').classList.remove('dark');
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
