957dbbb5789dd3bfc4f12098e2fbf2611c22a153
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-25 11:59:37 +0200

    js done

--- src/js/empty-lib-plug.js
+++ src/js/empty-lib-plug.js
@@ -1,5 +1,6 @@
 import { refs } from './refs';
 import { goHome } from './header';
+import { onQueueClick } from './header';
 const gif = './images/film.gif" alt="Film-GIF';
 
 const watchedGIF = 'https://i.gifer.com/gg7.gif';
@@ -8,7 +9,7 @@ const queueGIF = 'https://i.gifer.com/C4j.gif';
 const getEmptyWatchedMarkup = GIF => {
   return `
     <div class="plug__container">
-      <strong>Ooops! Is nothing here yet!</strong>
+      <strong class="plug__strong">Ooops! Is nothing here yet!</strong>
       <img class="plug__gif" src="${GIF}" />
       <p class="plug__text">Hurry up and watch a movie!</p>
       <button type="button" class="plug__button" data-action="go-home">
@@ -24,7 +25,7 @@ const getEmptyWatchedMarkup = GIF => {
 const getEmptyQueueMarkup = GIF => {
   return `
     <div class="plug__container">
-      <strong>Ooops! Is nothing here yet!</strong>
+      <strong class="plug__strong">Ooops! Is nothing here yet!</strong>
       <img class="plug__gif" src="${GIF}" />
       <p class="plug__text">Hurry up and add the movie!</p>
       <button type="button" class="plug__button" data-action="go-home">
@@ -40,6 +41,7 @@ const onGoHomeButtonClick = e => {
 
 const onGoQueueButtonClick = e => {
   console.log('CLICK!');
+  onQueueClick(e);
 };
 
 export const emptyWatchedRender = () => {
